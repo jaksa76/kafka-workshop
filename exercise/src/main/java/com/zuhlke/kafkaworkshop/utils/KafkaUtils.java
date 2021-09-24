98fd3629566ae2986d616da866d4d8c424ffb72e
Jaksa Vuckovic <jaksa.vuckovic@zuhlke.com>
2021-09-24 15:25:13 +0000

    merge changes from teaching

--- exercise/src/main/java/com/zuhlke/kafkaworkshop/utils/KafkaUtils.java
+++ exercise/src/main/java/com/zuhlke/kafkaworkshop/utils/KafkaUtils.java
@@ -4,11 +4,17 @@ import java.net.InetAddress;
 
 public class KafkaUtils {
     public static final String BOOTSTRAP_SERVERS = "workshop-kafka.kafka:9092";
+    
     public static String hostname() {
         try {
             return InetAddress.getLocalHost().getHostName();
         } catch (Exception e) {
             return "localhost";
         }
+    }
+
+    public static String studentName() {
+        String hostname = hostname();
+        return hostname.substring(0, hostname.indexOf("-"));
     }
 }
