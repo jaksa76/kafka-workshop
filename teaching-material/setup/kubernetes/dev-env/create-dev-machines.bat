6958389c2edd4bb43a0975b2a02a85a8cbeee44b
Jaksa Vuckovic <jaksa.vuckovic@zuhlke.com>
2022-10-20 08:49:24 +0200

    automated git cloning

--- teaching-material/setup/kubernetes/dev-env/create-dev-machines.bat
+++ teaching-material/setup/kubernetes/dev-env/create-dev-machines.bat
@@ -1,3 +1,4 @@
-helm install dev-machines .
+helm install dev-machines . --wait
+kubectl get pods -o name | foreach { kubectl exec $_ -- git clone https://github.com/jaksa76/kafka-workshop.git /root/kafka-workshop }
 kubectl get svc
 kubectl get pods -o name | foreach { echo $_; kubectl exec $_ -- cat /root/.config/code-server/config.yaml }
